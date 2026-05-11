5dbe090315a231b1c2be26f722d28573627e9de3
Jacob McGrath <jacob.c.mcgrath@gmail.com>
2026-05-11 16:40:36 -0700

    HORRIBLE. add more preprocessor defs with guards
    should definitely be a constexpr in some random header file and not a macro

--- LASS/src/Partial.cpp
+++ LASS/src/Partial.cpp
@@ -33,6 +33,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Track.h"
 #include "Iterator.h"
 
+#ifndef M_PI
+#define M_PI		3.14159265358979323846
+#endif
+
 //----------------------------------------------------------------------------//
 Partial::Partial()
 {
