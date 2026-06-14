79d159628e8aa87196dc45ab1e833327d4f9382c
Molly-Q <leyi4@illinois.edu>
2026-06-14 16:18:43 -0500

    Add (Optional) after Modifier group; set REV_Simple room size default value to 0.5

--- LASSIE/src/dialogs/functions/impl/RevFunctionBase.cpp
+++ LASSIE/src/dialogs/functions/impl/RevFunctionBase.cpp
@@ -92,6 +92,10 @@ void RevFunctionBase::rebuildDefaults() {
         first->setTitle(QStringLiteral("Sound"));
         first->hideButtons();
     }
+
+    if (m_mode == 0) {  //REV_Simple 
+        first->setRoomSizeText(QStringLiteral("0.5"));
+    } 
 }
 
 void RevFunctionBase::updateLabels() {
